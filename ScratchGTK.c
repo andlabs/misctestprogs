96b30aacaf6b07873e28819a919866709efd3df7
Pietro Gagliardi <pietro10@mac.com>
2014-04-16 23:17:07 -0400

    Added a scratch GTK+ program to base future ones off.

---
+++ ScratchGTK.c
@@ -0,0 +1,87 @@
+// scratch GTK+ program by pietro gagliardi 16 april 2014
+// uses code from 31 march 2014 and 15 april 2014
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#define GLIB_VERSION_MIN_REQUIRED GLIB_VERSION_2_32
+#define GLIB_VERSION_MAX_ALLOWED GLIB_VERSION_2_32
+#define GDK_VERSION_MIN_REQUIRED GDK_VERSION_3_4
+#define GDK_VERSION_MAX_ALLOWED GDK_VERSION_3_4
+#include <gtk/gtk.h>
+
+// flag variables here; use types gbooleean, gchar *, gint, gdouble
+
+#define flagBool(name, help) { #name, 0, 0, G_OPTION_ARG_NONE, &name, help, NULL }
+#define flagString(name, help) { #name, 0, 0, G_OPTION_ARG_STRING, &name, help, NULL }
+#define flagInt(name, help) { #name, 0, 0, G_OPTION_ARG_INT, &name, help, NULL }
+#define flagDouble(name, help) { #name, 0, 0, G_OPTION_ARG_DOUBLE, &name, help, NULL }
+static GOptionEntry flags[] = {
+	// options here
+	{ NULL, 0, 0, 0, NULL, NULL, NULL },
+};
+
+void init(int *argc, char *(*argv[]));
+
+char *args = "";		// other command-line arguments here, if any
+
+gboolean parseArgs(int argc, char *argv[])
+{
+	// parse arguments here, if any
+	// return TRUE if parsed successfully; FALSE otherwise
+	// (returning FALSE will show usage and quit)
+	return TRUE;
+}
+
+GtkGrid *buildUI(void)
+{
+	GtkGrid *grid;
+
+	grid = (GtkGrid *) gtk_grid_new();
+
+#define ADD(widget, x, y, xspan) gtk_grid_attach(grid, (GtkWidget *) (widget), y, x, xspan, 1)
+	// call ADD() for each widget
+
+	return grid;
+}
+
+int main(int argc, char *argv[])
+{
+	GtkWindow *mainwin;
+	GtkGrid *layout;
+
+	init(&argc, &argv);
+
+	mainwin = (GtkWindow *) gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(mainwin, "Main Window");
+	g_signal_connect(mainwin, "delete-event", gtk_main_quit, NULL);
+
+	layout = buildUI();
+	gtk_container_add((GtkContainer *) mainwin, (GtkWidget *) layout);
+	gtk_widget_show_all((GtkWidget *) mainwin);
+
+	gtk_main();
+	return 0;
+}
+
+void init(int *argc, char *(*argv[]))
+{
+	GError *flagserr = NULL;
+	GOptionContext *flagscontext;
+
+	flagscontext = g_option_context_new(args);
+	g_option_context_add_main_entries(flagscontext, flags, NULL);
+	// the next line also initializes GTK+
+	g_option_context_add_group(flagscontext, gtk_get_option_group(TRUE));
+	if (g_option_context_parse(flagscontext, argc, argv, &flagserr) != TRUE) {
+		fprintf(stderr, "error: %s\n", flagserr->message);
+		goto usage;
+	}
+
+	if (parseArgs(*argc, *argv) == TRUE)
+		return;
+	// otherwise fall through
+
+usage:
+	fprintf(stderr, "%s\n", g_option_context_get_help(flagscontext, FALSE, NULL));
+	exit(1);
+}
