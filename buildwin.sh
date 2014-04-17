e753b7f57de7efe9921864dc486297841f80c085
Pietro Gagliardi <pietro10@mac.com>
2014-04-17 17:18:54 -0400

    Added shell32.dll and advapi32.dll to the Windows build script (just to be safe).

--- buildwin.sh
+++ buildwin.sh
@@ -2,4 +2,4 @@
 	CC="x86_64-w64-mingw32-gcc"
 	shift
 }
-${CC:-i686-w64-mingw32-gcc} -g -o `basename "$1" .c`.exe "$@" --std=c99 -luser32 -lkernel32 -lcomctl32 -lcomdlg32 -lgdi32 -lmsimg32
+${CC:-i686-w64-mingw32-gcc} -g -o `basename "$1" .c`.exe "$@" --std=c99 -luser32 -lkernel32 -lcomctl32 -lcomdlg32 -lgdi32 -lmsimg32 -lshell32 -ladvapi32
