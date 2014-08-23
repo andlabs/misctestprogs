b280d036732a6e49cf143d0df23f8a1caa785ac7
Pietro Gagliardi <pietro10@mac.com>
2014-08-22 23:13:26 -0400

    Added program for testing the Breaking AppKit blog post's terminate: override.

---
+++ cocoaAppTerminate.m
@@ -0,0 +1,244 @@
+// 22 august 2014
+// scratch program 28 may 2014
+// borrows some things from the blog GTK+ scratch program (see that + its history)
+#import <stdio.h>
+#import <stdlib.h>
+#import <getopt.h>
+#import <Cocoa/Cocoa.h>
+
+// string helpers
+#define toNSString(x) ([NSString stringWithUTF8String:(x)])
+#define fromNSString(x) ([(x) UTF8String])
+
+// command line flags go here
+// the array format is @[NSString *name, NSString *help, NSValue *arg]
+// name should not have a leading - or --
+// arg should be @YES or @NO to determine if an argument is taken
+NSArray *flagspec(void)
+{
+	return @[
+		// args here
+		// no need for -help
+	];
+}
+
+// after init(), this will contain all the above flag names as keys with NSValue values
+// - if the flag takes no argument, the value is either @YES or @NO
+// - if the flag takes an argument, the value will be a pointer or NULL if not set
+NSMutableDictionary *flags;
+
+// after init(), this will contain the rest of the arguments
+NSMutableArray *otherArgs;
+
+// this is for the usage text
+// name other command-line arguments here, if any
+// for example, "[files...]"
+char *args = "";
+
+BOOL parseArgs()
+{
+	// parse arguments here, if any, from otherArgs
+	// return YES if parsed successfully; NO otherwise
+	// (returning NO will show usage and quit)
+	return YES;
+}
+
+@interface AppDelegate : NSObject
+@end
+
+AppDelegate *appDelegate;
+NSWindow *mainwin;
+
+@implementation AppDelegate
+
+// implementation methods here
+
+@end
+
+#define sysFontSize(x) [NSFont systemFontSizeForControlSize:(NS ## x ## ControlSize)]
+
+void buildUI(void)
+{
+#define SETFONT(c, size) [(c) setFont:[NSFont systemFontOfSize:sysFontSize(size)]]
+#define RESIZE(c, mask) [(c) setAutoresizingMask:(mask)]
+#define ADD(c) [[mainwin contentView] addSubview:(c)]
+	// use SETFONT(Regular/Small/Mini), RESIZE() and ADD()
+	// the mask argument to RESIZE() is one of the NSView resizing masks
+
+	// add widgets here
+}
+
+void init(int, char *[]);
+
+@interface OurApplication : NSApplication
+@end
+
+@implementation OurApplication : NSApplication
+
+- (void)terminate:(id)sender
+{
+	NSEvent *e;
+
+	[NSApp stop:NSApp];
+	e = [NSEvent otherEventWithType:NSApplicationDefined
+		location:NSZeroPoint
+		modifierFlags:0
+		timestamp:[[NSProcessInfo processInfo] systemUptime]
+		windowNumber:0
+		context:[NSGraphicsContext currentContext]
+		subtype:0
+		data1:0
+		data2:0];
+	[NSApp postEvent:e atStart:NO];
+}
+
+@end
+
+int main(int argc, char *argv[])
+{
+	init(argc, argv);
+	[OurApplication sharedApplication];
+	if ([NSApp setActivationPolicy:NSApplicationActivationPolicyRegular] != YES) {
+		fprintf(stderr, "error activating Cocoa application\n");
+		return 1;
+	}
+	[NSApp activateIgnoringOtherApps:TRUE];
+	appDelegate = [AppDelegate new];
+	[NSApp setDelegate:appDelegate];
+
+	// from http://www.cocoawithlove.com/2010/09/minimalist-cocoa-programming.html
+	NSMenu *menubar = [NSMenu new];
+	NSMenuItem *appMenuItem = [NSMenuItem new];
+	[menubar addItem:appMenuItem];
+	[NSApp setMainMenu:menubar];
+	NSMenu *appMenu = [NSMenu new];
+	NSString *appName = [[NSProcessInfo processInfo] processName];
+	NSString *quitTitle = [@"Quit " stringByAppendingString:appName];
+	NSMenuItem *quitMenuItem = [[NSMenuItem alloc] initWithTitle:quitTitle
+		action:@selector(terminate:)
+		keyEquivalent:@"q"];
+	[appMenu addItem:quitMenuItem];
+	[appMenuItem setSubmenu:appMenu];
+
+	[NSApp run];
+	NSLog(@"outside OurApplication");
+	return 0;
+}
+
+@implementation AppDelegate (usuallyTheSame)
+
+- (void)applicationDidFinishLaunching:(NSNotification *)note
+{
+	mainwin = [[NSWindow alloc]
+		initWithContentRect:NSMakeRect(300, 300, 320, 240)
+		styleMask:(NSTitledWindowMask | NSClosableWindowMask |
+			NSMiniaturizableWindowMask | NSResizableWindowMask)
+		backing:NSBackingStoreBuffered
+		defer:YES];
+	[mainwin setTitle:@"Main Window"];
+	[[mainwin contentView] setAutoresizesSubviews:YES];
+	buildUI();
+	[mainwin makeKeyAndOrderFront:self];
+}
+
+- (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)app
+{
+	NSLog(@"in applicationShouldTerminate");
+	return NSTerminateNow;
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)app
+{
+	NSLog(@"in applicationShouldTerminateAfterLastWindowClosed");
+	return YES;
+}
+
+@end
+
+void init(int argc, char *argv[])
+{
+	struct option *sopts;
+	size_t soptssize;
+	NSUInteger i;
+	NSArray *fspec;
+	int c;
+	int soptsindex;
+	int usageret = 1;
+
+	fspec = flagspec();
+	soptssize = ([fspec count] + 2) * sizeof (struct option);
+	sopts = (struct option *) malloc(soptssize);
+	if (sopts == NULL) {
+		fprintf(stderr, "memory exhausted processing command-line arguments\n");
+		exit(1);
+	}
+	memset(sopts, 0, soptssize);
+	flags = [NSMutableDictionary new];
+	for (i = 0; i < [fspec count]; i++) {
+		NSArray *flag;
+
+		flag = fspec[i];
+		sopts[i].name = fromNSString(flag[0]);
+		sopts[i].has_arg = no_argument;
+		flags[flag[0]] = @NO;
+		if ([flag[2] boolValue] == YES) {
+			sopts[i].has_arg = required_argument;
+			flags[flag[0]] =
+				[NSValue valueWithPointer:NULL];
+		}
+		// leave sopts[i].flag as NULL and sopts[i].val as 0
+	}
+	sopts[i].name = "help";
+	sopts[i].has_arg = no_argument;
+	sopts[i].val = 'h';		// leave sopts[i].flag as NULL
+
+	for (;;) {
+		c = getopt_long_only(argc, argv, "", sopts, &soptsindex);
+		if (c == -1)		// done
+			break;
+		switch (c) {
+		case 0:			// one of our options
+			if (sopts[soptsindex].has_arg == no_argument)
+				flags[fspec[soptsindex][0]] = @YES;
+			else
+				flags[fspec[soptsindex][0]] =
+					[NSValue valueWithPointer:optarg];
+			continue;
+		case 'h':			// -help
+			usageret = 0;
+			goto usage;
+		case '?':			// unknown
+			// getopt_long_only() will print the error
+			goto usage;
+		}
+		fprintf(stderr, "internal error: getopt_long_only() returned %d\n", c);
+		exit(1);
+	}
+
+	otherArgs = [NSMutableArray new];
+	for (i = (NSUInteger) optind; i < (NSUInteger) argc; i++)
+		[otherArgs addObject:toNSString(argv[i])];
+	if (!parseArgs())
+		goto usage;
+
+	return;
+
+usage:
+	fprintf(stderr, "usage: %s [options]", argv[0]);
+	if (args != NULL && *args != '\0')
+		fprintf(stderr, " %s", args);
+	fprintf(stderr, "\n");
+	for (i = 0; i < [fspec count]; i++) {
+		NSArray *flag;
+		char *opt;
+
+		flag = fspec[i];
+		opt = "";
+		if ([flag[2] boolValue] == YES)
+			opt = " arg";
+		fprintf(stderr, "\t-%s%s: %s\n",
+			fromNSString(flag[0]), opt,
+			fromNSString(flag[1]));
+	}
+	exit(usageret);
+}
