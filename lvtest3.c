420deb19aab8bc6f150140cd1a35203f51a63829
Pietro Gagliardi <pietro10@mac.com>
2015-04-04 15:45:56 -0400

    More lvtest3.c work.

--- lvtest3.c
+++ lvtest3.c
@@ -42,16 +42,12 @@ static HMODULE comctl32;
 // note that this is an 8-bit character string we're writing; see the encoding clause
 static const char manifest[] = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<assembly xmlns=\"urn:schemas-microsoft-com:asm.v1\" manifestVersion=\"1.0\">\n<assemblyIdentity\n    version=\"1.0.0.0\"\n    processorArchitecture=\"*\"\n    name=\"CompanyName.ProductName.YourApplication\"\n    type=\"win32\"\n/>\n<description>Your application description here.</description>\n<dependency>\n    <dependentAssembly>\n        <assemblyIdentity\n            type=\"win32\"\n            name=\"Microsoft.Windows.Common-Controls\"\n            version=\"6.0.0.0\"\n            processorArchitecture=\"*\"\n            publicKeyToken=\"6595b64144ccf1df\"\n            language=\"*\"\n        />\n    </dependentAssembly>\n</dependency>\n</assembly>\n";
 
-/*
-Windows requires a manifest file to enable Common Controls version 6.
-The only way to not require an external manifest is to synthesize the manifest ourselves.
-We can use the activation context API to load it at runtime.
-References:
-- http://stackoverflow.com/questions/4308503/how-to-enable-visual-styles-without-a-manifest
-- http://support.microsoft.com/kb/830033
-Because neither Go nor MinGW have ways to compile in resources like this (as far as I know), we have to do the work ourselves.
-*/
-DWORD initCommonControls(char **errmsg)
+void die(char *s)
+{
+	// TODO
+}
+
+void initCommonControls(BOOL comctl6)
 {
 	WCHAR temppath[MAX_PATH + 1];
 	WCHAR filename[MAX_PATH + 1];
@@ -64,53 +60,33 @@ DWORD initCommonControls(char **errmsg)
 	// this is listed as WINAPI in both Microsoft's and MinGW's headers, but not on MSDN for some reason
 	BOOL (*WINAPI ficc)(const LPINITCOMMONCONTROLSEX);
 
-	if (GetTempPathW(MAX_PATH + 1, temppath) == 0) {
-		*errmsg = "error getting temporary path for writing manifest file";
-		return GetLastError();
-	}
-	if (GetTempFileNameW(temppath, L"manifest", 0, filename) == 0) {
-		*errmsg = "error getting temporary filename for writing manifest file";
-		return GetLastError();
-	}
-	file = CreateFileW(filename, GENERIC_WRITE,
-		0,			// don't share while writing
-		NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (file == NULL) {
-		*errmsg = "error creating manifest file";
-		return GetLastError();
-	}
-	nExpected = (sizeof manifest / sizeof manifest[0]) - 1;		// - 1 to omit the terminating null character)
-	SetLastError(0);		// catch errorless short writes
-	if (WriteFile(file, manifest, nExpected, &nGot, NULL) == 0) {
-		*errmsg = "error writing manifest file";
-		return GetLastError();
-	}
-	if (nGot != nExpected) {
-		DWORD lasterr;
+	if (comctl6) {
+		if (GetTempPathW(MAX_PATH + 1, temppath) == 0)
+			die("getting temporary path for writing manifest file");
+		if (GetTempFileNameW(temppath, L"manifest", 0, filename) == 0)
+			die("getting temporary filename for writing manifest file");
+		file = CreateFileW(filename, GENERIC_WRITE,
+			0,			// don't share while writing
+			NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (file == NULL)
+			die("creating manifest file");
+		nExpected = (sizeof manifest / sizeof manifest[0]) - 1;		// - 1 to omit the terminating null character)
+		if (WriteFile(file, manifest, nExpected, &nGot, NULL) == 0)
+			die("writing manifest file");
+		if (nGot != nExpected)
+			die("short write to manifest file");
+		if (CloseHandle(file) == 0)
+			die("closing manifest file (this IS an error here because not doing so will prevent Windows from being able to use the manifest file in an activation context)");
 
-		lasterr = GetLastError();
-		*errmsg = "short write to manifest file";
-		if (lasterr == 0)
-			*errmsg = "short write to manifest file without error code";
-		return lasterr;
-	}
-	if (CloseHandle(file) == 0) {
-		*errmsg = "error closing manifest file (this IS an error here because not doing so will prevent Windows from being able to use the manifest file in an activation context)";
-		return GetLastError();
-	}
-
-	ZeroMemory(&actctx, sizeof (ACTCTX));
-	actctx.cbSize = sizeof (ACTCTX);
-	actctx.dwFlags = ACTCTX_FLAG_SET_PROCESS_DEFAULT;
-	actctx.lpSource = filename;
-	ac = CreateActCtx(&actctx);
-	if (ac == INVALID_HANDLE_VALUE) {
-		*errmsg = "error creating activation context for synthesized manifest file";
-		return GetLastError();
-	}
-	if (ActivateActCtx(ac, &comctlManifestCookie) == FALSE) {
-		*errmsg = "error activating activation context for synthesized manifest file";
-		return GetLastError();
+		ZeroMemory(&actctx, sizeof (ACTCTX));
+		actctx.cbSize = sizeof (ACTCTX);
+		actctx.dwFlags = ACTCTX_FLAG_SET_PROCESS_DEFAULT;
+		actctx.lpSource = filename;
+		ac = CreateActCtx(&actctx);
+		if (ac == INVALID_HANDLE_VALUE)
+			die("creating activation context for synthesized manifest file");
+		if (ActivateActCtx(ac, &comctlManifestCookie) == FALSE)
+			die("activating activation context for synthesized manifest file");
 	}
 
 	ZeroMemory(&icc, sizeof (INITCOMMONCONTROLSEX));
@@ -118,28 +94,15 @@ DWORD initCommonControls(char **errmsg)
 	icc.dwICC = wantedICCClasses;
 
 	comctl32 = LoadLibraryW(L"comctl32.dll");
-	if (comctl32 == NULL) {
-		*errmsg = "error loading comctl32.dll";
-		return GetLastError();
-	}
-
-	// GetProcAddress() only takes a multibyte string
-#define LOAD(fn) f = GetProcAddress(comctl32, fn); \
-	if (f == NULL) { \
-		*errmsg = "error loading " fn "()"; \
-		return GetLastError(); \
-	}
-
-	LOAD("InitCommonControlsEx");
+	if (comctl32 == NULL)
+		die("loading comctl32.dll");
+	f = GetProcAddress(comctl32, "InitCommonControlsEx");
+	if (f == NULL)
+		die("loading InitCommonControlsEx()");
 	ficc = (BOOL (*WINAPI)(const LPINITCOMMONCONTROLSEX)) f;
-	if ((*ficc)(&icc) == FALSE) {
-		*errmsg = "error initializing Common Controls (comctl32.dll)";
-		return GetLastError();
-	}
-
-	return 0;
+	if ((*ficc)(&icc) == FALSE)
+		die("initializing Common Controls (comctl32.dll)");
 }
-
 HFONT msgfont;
 
 HWND lv = NULL;
@@ -195,7 +158,7 @@ BOOL create(HWND hwnd, LPCREATESTRUCT lpcs)
 #define LVTOP (MARGIN + BTNHT + PADDING)
 	lv = CreateWindowExW(lvexstyle,
 		WC_LISTVIEWW, L"Main Window",
-		LVS_REPORT | LVS_NOSORTHEADER | LVS_SHOWSELALWAYS | LVS_SINGLESEL | WS_CHILD | WS_HSCROLL | WS_VSCROLL | WS_VISIBLE | lvstyle,
+		LVS_REPORT | LVS_NOSORTHEADER | LVS_SHOWSELALWAYS | WS_CHILD | WS_HSCROLL | WS_VSCROLL | WS_VISIBLE | lvstyle,
 		MARGIN, LVTOP,
 		WINWIDTH - (2 * MARGIN), WINHEIGHT - LVTOP - MARGIN,
 		hwnd, NULL, GetModuleHandle(NULL), NULL);
@@ -296,13 +259,13 @@ int main(int argc, char *argv[])
 	int nCmdShow;
 	STARTUPINFOW si;
 	MSG msg;
-	char *m;
 	RECT winr;
 	NONCLIENTMETRICSW ncm;
 	int i;
 	BOOL comctl6 = TRUE;
 	DWORD edges = 0;
 	BOOL noclientedge = FALSE;
+	BOOL multisel = FALSE;
 
 	for (i = 1; i < argc; i++)
 		if (strcmp(argv[i], "windowedge") == 0)
@@ -329,6 +292,8 @@ int main(int argc, char *argv[])
 			custommargin = TRUE;
 		else if (strcmp(argv[i], "hotlabels") == 0)
 			hotlabels = TRUE;
+		else if (strcmp(argv[i], "multisel") == 0)
+			multisel = TRUE;
 		else {
 			fprintf(stderr, "unknown option %s\n", argv[i]);
 			return 1;
@@ -338,13 +303,15 @@ int main(int argc, char *argv[])
 		edges |= WS_EX_CLIENTEDGE;
 	lvexstyle |= edges;
 
+	if (!multisel)
+		lvstyle |= LVS_SINGLESEL;
+
 	ZeroMemory(&ncm, sizeof (NONCLIENTMETRICSW));
 	ncm.cbSize = sizeof (NONCLIENTMETRICSW);
 	SystemParametersInfoW(SPI_GETNONCLIENTMETRICS, sizeof (NONCLIENTMETRICSW), &ncm, sizeof (NONCLIENTMETRICSW));
 	msgfont = CreateFontIndirectW(&(ncm.lfMessageFont));
 
-	if (comctl6)
-		initCommonControls(&m);
+	initCommonControls(comctl6);
 
 	ZeroMemory(&wc, sizeof (WNDCLASSW));
 	wc.lpszClassName = L"mainwin";
