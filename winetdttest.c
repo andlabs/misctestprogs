2af236e01c5841039e51342cb9a3511b669e8422
Pietro Gagliardi <pietro10@mac.com>
2015-05-30 14:24:31 -0400

    More work.

--- winetdttest.c
+++ winetdttest.c
@@ -36,6 +36,13 @@ LRESULT CALLBACK wndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return DefWindowProcW(hwnd, uMsg, wParam, lParam);
 }
 
+INT_PTR CALLBACK dlgproc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (uMsg == WM_INITDIALOG)
+		return TRUE;
+	return FALSE;
+}
+
 int main(int argc, char *argv[])
 {
 	WNDCLASSW wc;
@@ -57,11 +64,34 @@ int main(int argc, char *argv[])
 		400, 400,
 		NULL, NULL, GetModuleHandle(NULL), NULL);
 
-	dialog = CreateWindowExW(WS_EX_CONTROLPARENT,
-		WC_DIALOG, L"",
-		WS_CHILD | WS_VISIBLE,
-		100, 100, 200, 200,
-		mainwin, NULL, GetModuleHandle(NULL), NULL);
+	if (argc > 1)
+		dialog = CreateWindowExW(WS_EX_CONTROLPARENT,
+			WC_DIALOG, L"",
+			WS_CHILD | WS_VISIBLE,
+			100, 100, 200, 200,
+			mainwin, NULL, GetModuleHandle(NULL), NULL);
+	else {
+		const BYTE dlgtemplate[] = {
+			0x01, 0x00,				// version
+			0xFF, 0xFF,				// signature
+			0x00, 0x00, 0x00, 0x00,		// help
+			0x00, 0x00, 0x01, 0x00,		// WS_EX_CONTROLPARENT
+			0x00, 0x00, 0x00, 0x50,		// WS_CHILD | WS_VISIBLE
+			0x00, 0x00,				// no controls
+			100, 0,					// X/Y/Width/Height
+			100, 0,
+			100, 0,
+			100, 0,
+			0x00, 0x00,				// menu
+			0x00, 0x00,				// class
+			0x00, 0x00,				// title
+			0x00, 0x00, 0x00, 0x00,		// some padding
+			0x00, 0x00, 0x00, 0x00,		// more padding
+			0x00, 0x00, 0x00, 0x00,		// just to be safe
+		};
+
+		dialog = CreateDialogIndirectW(GetModuleHandle(NULL), (LPCDLGTEMPLATEW) dlgtemplate, mainwin, dlgproc);
+	}
 	printf("%I32X\n", EnableThemeDialogTexture(dialog, ETDT_ENABLE | ETDT_USETABTEXTURE | ETDT_ENABLETAB));
 
 	ShowWindow(mainwin, SW_SHOWDEFAULT);
