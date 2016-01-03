8ab9aa663e67ad7233e7a402b5111e706abb9605
Pietro Gagliardi <pietro10@mac.com>
2016-01-03 02:15:16 -0500

    More programs and Swift 2 fixes.

--- macsplitviewtest.swift
+++ macsplitviewtest.swift
@@ -1,4 +1,5 @@
-// 17 august 2015
+// 3 january 2016
+// scratch program 17 august 2015
 import Cocoa
 
 var keepAliveMainwin: NSWindow? = nil
@@ -10,6 +11,34 @@ func appLaunched() {
 		backing: NSBackingStoreType.Buffered,
 		`defer`: true)
 	let contentView = mainwin.contentView!
+
+	let splitView = NSSplitView(frame: NSZeroRect)
+	splitView.dividerStyle = NSSplitViewDividerStyle.Thin
+	splitView.vertical = true
+	splitView.translatesAutoresizingMaskIntoConstraints = false
+	contentView.addSubview(splitView)
+
+	let box1 = NSBox(frame: NSZeroRect)
+	box1.boxType = NSBoxType.Custom
+	box1.borderType = NSBorderType.LineBorder
+	box1.fillColor = NSColor(forControlTint: NSControlTint.GraphiteControlTint)
+	box1.translatesAutoresizingMaskIntoConstraints = false
+	splitView.addSubview(box1)
+
+	let box2 = NSBox(frame: NSZeroRect)
+	box2.boxType = NSBoxType.Custom
+	box2.borderType = NSBorderType.LineBorder
+	box2.fillColor = NSColor(forControlTint: NSControlTint.BlueControlTint)
+	box2.translatesAutoresizingMaskIntoConstraints = false
+	splitView.addSubview(box2)
+
+	let views: [String: NSView] = [
+		"splitView":	splitView,
+	]
+	addConstraint(contentView, "H:|-[splitView]-|", views)
+	addConstraint(contentView, "V:|-[splitView]-|", views)
+
+	splitView.setPosition(50, ofDividerAtIndex: 0)
 
 	mainwin.cascadeTopLeftFromPoint(NSMakePoint(20, 20))
 	mainwin.makeKeyAndOrderFront(mainwin)
