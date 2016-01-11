facf226283502c2ebba63410617ddb772e33ae18
Pietro Gagliardi <pietro10@mac.com>
2016-01-11 18:56:29 -0500

    More font work.

--- macfonttest.swift
+++ macfonttest.swift
@@ -79,3 +79,59 @@ for (name, props) in results {
 	let paddedName = name.stringByPaddingToLength(maxlen, withString: " ", startingAtIndex: 0)
 	print(paddedName + " | " + props)
 }
+
+print("")
+
+print("small caps")
+func has(selectors: [NSDictionary], _ what: Int) -> Bool {
+	for s in selectors {
+		let k = s.objectForKey(kCTFontFeatureSelectorIdentifierKey) as! Int
+		if k == what {
+			return true
+		}
+	}
+	return false
+}
+results = [:]
+maxlen = 0
+for desc: NSFontDescriptor in (collection.matchingDescriptors ?? []) {
+	var name = desc.objectForKey(NSFontNameAttribute) as! String
+	name += " " + (desc.objectForKey(NSFontFamilyAttribute) as! String)
+	let list = desc.objectForKey(kCTFontFeaturesAttribute as String)
+	var props = ""
+	if list != nil {
+		for entry in (list as! [NSDictionary]) {
+			let typeID = entry.objectForKey(kCTFontFeatureTypeIdentifierKey) as! Int
+			let selectors = entry.objectForKey(kCTFontFeatureTypeSelectorsKey) as! [NSDictionary]
+			switch typeID {
+			case kLetterCaseType:
+				if has(selectors, kSmallCapsSelector) {
+					props += " kLetterCaseType"
+				}
+			case kLowerCaseType:
+				if has(selectors, kLowerCaseSmallCapsSelector) {
+					props += " kLowerCaseType"
+				}
+			case kUpperCaseType:
+				if has(selectors, kUpperCaseSmallCapsSelector) {
+					props += " kUpperCaseType"
+				}
+			// because programmers and language designers are stupid in general
+			// it's just that swift ran with it
+			default:
+				break
+			}
+		}
+	}
+	if props == "" {
+		continue
+	}
+	results[name] = props.substringFromIndex(props.startIndex.advancedBy(1))
+	if maxlen < name.characters.count {
+		maxlen = name.characters.count
+	}
+}
+for (name, props) in results {
+	let paddedName = name.stringByPaddingToLength(maxlen, withString: " ", startingAtIndex: 0)
+	print(paddedName + " | " + props)
+}
